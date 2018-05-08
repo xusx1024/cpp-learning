59fda19b589c5e7bbccca1e67d61ef44b3df7ee5
xushengxing <xusx1024@163.com>
2018-05-08 20:35:42 +0800

    数组

---
+++ 练习/3.27.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+// using std::string;
+using namespace std;
+string sa[10];
+int ia[10];
+
+int main(){
+/*
+int ia[4 * 7 - 14];
+unsigned buf_size = 1024;
+int ib[buf_size];
+*/
+
+string sa2[10];
+int ia2[10];
+  return 0;
+}
