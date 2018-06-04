acc575c9da8a348351ca77d2e356f14063b02d90
xushengxing <xusx1024@163.com>
2018-06-04 17:53:13 +0800

    10.3.2节练习同步

---
+++ 练习/10.3.2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+int main(int argc, char** argv)
+{
+
+/** 10.14
+[] (  int& a,   int& b)
+{return a+b;}
+*/
+
+/**
+10.15
+
+[x] (int & a){return a + x;}
+
+*/
+
+/**
+10.16
+
+
+
+*/
+
+  return 0;
+}
